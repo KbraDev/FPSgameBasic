24cd150fc9a65516203696e8511d62dea629c588
KbraDev <ltovar.dev@gmail.com>
2025-03-25 13:22:57 -0600

    codigo de jugador movible

---
+++ player/scripts/jugador/jugador.gd
@@ -0,0 +1,83 @@
+extends CharacterBody3D
+
+# Stats
+var saludAhora : int = 10
+var saludMax : int = 10
+var ammo : int = 15
+var score : int = 0
+
+# Physics
+var moveSpeed : float = 5.0
+var jumpForce : float = 4.0
+var gravity : float = 9.8
+
+# Camera
+var minLookAngle : float = -90.0
+var maxLookAngle : float = 90
+var cameraSens : float = .2
+
+# Vectors
+var vel : Vector3 = Vector3()
+var mouseDelta : Vector2 = Vector2()
+
+# Camera 
+@onready var camera = get_node("cameraOrbit/Camera3D")
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Captura el movimiento del mouse
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
+
+# Maneja el movimiento de la camara en correlacion al mouse
+func _process(delta: float) -> void:
+	# Rotar camara en el eje X
+	camera.rotation_degrees -= Vector3(rad_to_deg(mouseDelta.y),0,0) * cameraSens * delta
+	
+	# Limitar la rotacion vertical 
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
+	
+	# Rotar el jugador en el eje Y
+	rotation_degrees -= Vector3(0, rad_to_deg(mouseDelta.x),0) * cameraSens * delta
+	
+	# Reiniciar mouseDelta
+	mouseDelta = Vector2()
+	
+
+func _physics_process(delta: float) -> void:
+	# Reiniciar x y z de velocidad en cada cuadro
+	velocity.x = 0
+	velocity.z = 0
+	
+	var input = Vector2()
+	
+	# Input de movimientos
+	if Input.is_action_pressed("up"):
+		input.y -= 1
+	if Input.is_action_pressed("down"):
+		input.y += 1
+	if Input.is_action_pressed("left"):
+		input.x -= 1
+	if Input.is_action_pressed("right"):
+		input.x += 1
+		
+	input = input.normalized()
+	
+	var adelante = -global_transform.basis.z
+	var derecha = -global_transform.basis.x
+
+	# Poner velocidad
+	velocity.z = (adelante * input.y + derecha * input.x).z * moveSpeed
+	velocity.x = (adelante * input.y + derecha * input.x).x * moveSpeed
+	
+	# Aplicar gravedad
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jumpForce
+	
+	move_and_slide()
